605298fe551cfaad5980fcadb582e058f6e842d7
Ung Tuấn Lĩnh <84364281+unglinh279@users.noreply.github.com>
2022-08-20 15:49:56 +0700

    v3
    - Working text-base version of Wordle
    - Unlimited number of guesses

--- wordle.m
+++ wordle.m
@@ -12,15 +12,12 @@ while ischar(tline)
 end
 fclose(fid);
 
-% game start
-
-keepPlaying = true;
-
 % innitial answer
 anwser = wordData(randi(length(wordData)));
 
-% start guessing
+fprintf("Notes: \n 0: Wrong letter \n 1: Correct letter \n 2: Correct letter but wrong position \n");
 
+% start guessing
 disp(anwser);
 while true
     guess = "";
@@ -32,6 +29,29 @@ while true
     if strcmp(guess, anwser)
         break;
     end
+
+    output = zeros(1,5);
+    ansCheck = zeros(1, 5);
+
+    for i = 1:5
+        if extract(anwser, i) == extract(guess, i)
+            output(i) = 1;
+            ansCheck(i) = 1;
+        end
+    end
+
+    for i = 1:5
+        if output(i) ~= 1
+            for j = 1:5
+                if ~ansCheck(j) && extract(guess, i) == extract(anwser, j)
+                    output(i) = 2;
+                    ansCheck(j) = 1;
+                end
+            end
+        end
+    end
+
+    disp(output);
 end
 
 disp('Congratulation! The word is: ' + anwser);
