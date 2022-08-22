19eb11704f951f066c995f31f37e956cd2dca9a4
Ung Tuấn Lĩnh <84364281+unglinh279@users.noreply.github.com>
2022-08-22 21:40:00 +0700

    v7
    - Add "view definition" button when game end
    - Clarify comments & clean code

--- gui.m
+++ gui.m
@@ -24,25 +24,31 @@ guidata(hObject, handles);
 function varargout = gui_OutputFcn(hObject, eventdata, handles) 
 varargout{1} = handles.output;
 
-% INITIALIZE THE GAME
+% INITIALIZE THE GAME (PLAY BUTTON PRESSED)
 function startBtn_Callback(hObject, eventdata, handles)
 global display guessNum wordData answer;
 
 % clear all
 clc;
 
+% init visibility of some buttons / text
 set(handles.endTxt, 'visible', 'off');
 set(handles.ansTxt, 'visible', 'off');
 set(handles.guessInput, 'visible', 'on');
 set(handles.startBtn, 'visible', 'off');
+set(handles.checkWord, 'visible', 'off');
 
+% init guess num / guess row
 guessNum = 1;
+
+% init handles for all boxes
 display = [handles.dis11 handles.dis12 handles.dis13 handles.dis14 handles.dis15; ...
            handles.dis21 handles.dis22 handles.dis23 handles.dis24 handles.dis25; ...
            handles.dis31 handles.dis32 handles.dis33 handles.dis34 handles.dis35; ...
            handles.dis41 handles.dis42 handles.dis43 handles.dis44 handles.dis45; ...
            handles.dis51 handles.dis52 handles.dis53 handles.dis54 handles.dis55];
 
+% set boxes color to default
 for i = 1:5
     for j = 1:5
         set(display(i, j), 'String', '');
@@ -64,25 +70,28 @@ fclose(fid);
 % avoid same random numbers after startup
 rng('shuffle');
 
-% innitial answer
+% init answer
 answer = wordData(randi(length(wordData)));
 
-
-% every input
+% GUESSING (FOR EVERY WORD GUESS)
 function guessInput_Callback(hObject, eventdata, handles)
 global display guessNum wordData answer
 
-% START GUESSING
+% get input word
 guess = get(hObject, 'String');
 
+% validity check
 if length(guess) ~= 5
     msgbox("Please enter a 5 letter word")
 elseif ~ismember(guess, wordData)
-    msgbox("Word is not valid")
+    msgbox("Please enter a valid word")
 else
+    % init state of each letter
     output = zeros(1,5);
+    % save if the letter of answer is checked
     ansCheck = zeros(1, 5);
     
+    % check exact match
     for i = 1:5
         if extract(answer, i) == extract(guess, i)
             output(i) = 1;
@@ -90,9 +99,11 @@ else
         end
     end
     
+    % check exact match but still appear in answer
     for i = 1:5
         if output(i) ~= 1
             for j = 1:5
+                    % exclude already exact match
                 if ~ansCheck(j) && extract(guess, i) == extract(answer, j)
                     output(i) = 2;
                     ansCheck(j) = 1;
@@ -101,6 +112,7 @@ else
         end
     end
     
+    % display letter & change bg color base on state
     for i = 1:5
         set(display(guessNum, i), 'String', upper(extract(guess, i)));
         if output(i) == 1
@@ -114,6 +126,7 @@ else
     
     guessNum = guessNum + 1;
 
+    % end the game if get correct answer or > 5 turns
     if strcmp(guess, answer)
         gameEnd(1, handles);
     elseif guessNum > 5
@@ -121,14 +134,19 @@ else
     end
 end
 
+% clear input box
 set(handles.guessInput, 'String', '');
 
 function gameEnd(state, handles)
 global answer;
 
+% change visibility of some buttons / text
 set(handles.endTxt, 'visible', 'on');
 set(handles.ansTxt, 'visible', 'on');
+set(handles.checkWord, 'visible', 'on');
 set(handles.guessInput, 'visible', 'off');
+set(handles.startBtn, 'visible', 'on');
+set(handles.startBtn, 'String', "PLAY AGAIN");
 
 set(handles.ansTxt, 'String', "THE WORD IS: " + upper(answer));
 
@@ -142,12 +160,15 @@ else
     set(handles.endTxt, 'String', "YOU LOSE!");
 end
 
-set(handles.startBtn, 'visible', 'on');
-set(handles.startBtn, 'String', "PLAY AGAIN");
-
-
 % --- Executes during object creation, after setting all properties.
 function guessInput_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on button press in checkWord.
+function checkWord_Callback(hObject, eventdata, handles)
+global answer;
+% open dictionary for definition of answer
+web("https://www.dictionary.com/browse/" + answer);
